4f1c920a78845e1b49aa88e612696e6113da5c8c
Deepanshu <113329037+DeepanshuTyagi2004@users.noreply.github.com>
2025-10-29 21:57:31 +0530

    trpc setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
+import Client from "./client";
 
 
-export default async function Home() {
+const Page = async () => {
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.hello.queryOptions({ text: "HELLO" }))
   return (
-    <div >
-      Hello World
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <div >
+        <Suspense fallback={<p> Loading... </p>}>
+          <Client />
+        </Suspense>
+      </div>
+    </HydrationBoundary>
   );
 }
+
+export default Page;
