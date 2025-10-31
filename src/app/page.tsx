6f9400a23ed0c97bfaa23b74ac6db7aaee43a8cf
Deepanshu <113329037+DeepanshuTyagi2004@users.noreply.github.com>
2025-10-31 12:27:13 +0530

    Background AI Jobs - AgentKit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,25 @@
 "use client";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
 import { toast } from "sonner";
 
 
 const Page = () => {
+  const [value, setValue] = useState("");
   const trpc = useTRPC();
   const invoke = useMutation(trpc.invoke.mutationOptions({
     onSuccess: () => {
-      toast.success("Background Job Started")
+      toast.success("Background Job Started " + value)
     }
   }))
   return (
     <div className="p-4 max-w-7xl mx-auto">
+      <Input value={value} onChange={(e) => setValue(e.target.value)} />
       <Button onClick={() =>
-        invoke.mutate({ text: "Hello" })}>
+        invoke.mutate({ value: value })}>
         Invoke background job
       </Button>
     </div>
