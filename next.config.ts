7234661270a6ffcd35d2b93b82c5d3e98848a815
Deepanshu <113329037+DeepanshuTyagi2004@users.noreply.github.com>
2025-11-22 23:09:12 +0530

    targetbinary updated

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // eslint-disable-next-line @typescript-eslint/no-require-imports
+      const { PrismaPlugin } = require("@prisma/nextjs-monorepo-workaround-plugin");
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
